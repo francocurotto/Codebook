e412ddf99d20931fc3702a3fb8928b50a9bfcfa4
Franco Curotto <francocurotto@gmail.com>
2026-09-24 21:13:20 -0300

    first decypher successful

--- decryptor_tester.py
+++ decryptor_tester.py
@@ -1,34 +1,43 @@
 import argparse
 import pandas as pd
 
+# contstants
+BYTES = 128
+ALLOWED = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,'?! \n"
+
 # get inputs
 parser = argparse.ArgumentParser()
 parser.add_argument("filename")
 parser.add_argument("split", type=int)
-parser.add_argument("offset", type=int)
 args = parser.parse_args()
 
 # read file
 with open(args.filename, "rb") as file:
     byte_array = file.read()
 
-# get byte split
-byte_split = byte_array[args.offset::args.split]
+# iterate on every offset
+results = []
+for offset in range(args.split):
+    # get byte split
+    byte_split = byte_array[offset::args.split]
+    # test all possible decryption bytes
+    tests = []
+    keys = []
+    for key_byte in range(BYTES):
+        test = []
+        for cipher_byte in byte_split:
+            test_byte = cipher_byte ^ key_byte
+            if test_byte in ALLOWED:
+                test_char = bytearray([test_byte]).decode("utf-8")
+                test.append(test_char)
+        if len(test) == len(byte_split):
+            keys.append(key_byte)
+            tests.append(test)
 
-# test all possible decryption bytes
-BYTES = 256
-tests = []
-for key_byte in range(BYTES):
-    test = []
-    for cipher_byte in byte_split:
-        test_byte = cipher_byte ^ key_byte
-        if test_byte < 0x20 or test_byte > 0x7e:
-            test_char = "❌"
-        else:
-            test_char = bytearray([test_byte]).decode("utf-8")
-        test.append(test_char)
-    tests.append(test)
+    # convert into csv
+    results.append(pd.DataFrame(tests, index=keys))
 
-# convert into csv
-result = pd.DataFrame(tests)
-result.to_csv(f"TEST_{args.filename}_{args.split}_{args.offset}.csv")
+excel = f"TEST_{args.filename}_{args.split}.xlsx"
+with pd.ExcelWriter(excel, engine="openpyxl") as writer:
+    for i, result in enumerate(results):
+        result.to_excel(writer, sheet_name=f"Offset {i + 1}")
